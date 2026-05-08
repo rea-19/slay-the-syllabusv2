69ec8daf8c78a2102e11c7fdad1d766853787de2
Your Name <reannejanelle@outlook.com>
2026-05-09 09:58:43 +1000

    added player, enemy, and card within the battle page.js

--- app/battle/page.js
+++ app/battle/page.js
@@ -6,10 +6,16 @@
 // attack cards
 // quiz popup
 
-
 "use client";
+import "../../styles/battle.css";
+
+import { useState } from "react";
 
 export default function BattlePage() {
+
+    const [playerHP, setPlayerHP] = useState(100);
+    const [enemyHP, setEnemyHP] = useState(100);
+
     return (
         <div className="battle-page">
             <h1>Slay the Syllabus</h1>
@@ -17,8 +23,21 @@ export default function BattlePage() {
             <div className="arena"></div>
                 <div className="player">
                     {/* add the player image!!!!!! */}
-                    <img src="" alt="player">
-                    </img>
+                    <img src="" alt="player"></img>
+                    <p>Player HP: {playerHP}</p>
+                </div>
+
+                <div className="enemy">
+                    {/* add the player image!!!!!! */}
+                    <img src="" alt="player"></img>
+                    <p>Enemy HP: {enemyHP}</p>
+                </div>
+
+                <div className="cards">
+                    <button>Light Attack</button>
+                    <button>Medium Attack</button>
+                    <button>Heavy Attack</button>
+                    <button>Ultimate Attack</button>
                 </div>
         </div>
     )
