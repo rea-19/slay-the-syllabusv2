bd0686a02a6414990fbf35ef476a346da781a006
Your Name <reannejanelle@outlook.com>
2026-05-09 09:53:37 +1000

    battle page added the basic html

--- app/battle/page.js
+++ app/battle/page.js
@@ -5,3 +5,21 @@
 // player/enemy
 // attack cards
 // quiz popup
+
+
+"use client";
+
+export default function BattlePage() {
+    return (
+        <div className="battle-page">
+            <h1>Slay the Syllabus</h1>
+
+            <div className="arena"></div>
+                <div className="player">
+                    {/* add the player image!!!!!! */}
+                    <img src="" alt="player">
+                    </img>
+                </div>
+        </div>
+    )
+}
