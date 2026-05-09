f1e5542def78ea4a4964694507cb11b2c18a5de0
Your Name <reannejanelle@outlook.com>
2026-05-09 14:08:12 +1000

    add the styling of the characters

--- app/battle/page.js
+++ app/battle/page.js
@@ -79,84 +79,109 @@ export default function BattlePage() {
 
     return (
         <div className="battle-page">
+            <div className="hud">
+                {/* player HUD */}
+                <div className="hud-player">
 
-
-                {/* arena */}
-            <div className="arena"></div>
-                <div className="hud">
-
-            {/* player HUD */}
-            <div className="hud-player">
-
-                <div className="hp-bar">
-                    <div
-                        className="hp-fill player-hp"
-                        style={{ width: `${playerHP}%` }}
-                    />
-                </div>
-                <h3>{player.name}</h3>
-
-                <p className="hp-text">{playerHP} HP</p>
-            </div>
-
-            <div className="vs">VS</div>
-
-            {/* enemy hud */}
-            <div className="hud-enemy">
-        
-                <div className="hp-bar">
-                    <div
-                        className="hp-fill enemy-hp"
-                        style={{ width: `${enemyHP}%` }}
-                    />
-                </div>
-                <h3>{enemy.name}</h3>
-
-                <p className="hp-text">{enemyHP} HP</p>
-
-            </div>
-
-        </div>
-
-                {/* cards */}
-
-                <div className="cards">
-    
-                    {cards.map((card, index) => (
-
-                    <div key={index} className="card outline outline-2 outline-black-400 hover:scale-105 transition">
-
-                        {/* IMAGE */}
-
-                        <Image
-                            src={card.icon}
-                            alt={card.name}
-                            width={100}
-                            height={140}
-
+                    <div className="hp-bar">
+                        <div
+                            className="hp-fill player-hp"
+                            style={{ width: `${playerHP}%` }}
                         />
-
-                        {/* ADD INFO TO CARD IMAGE */}
-                        <p>{card.name}</p>
-                        <p>{player.attack_damage + card.damageBonus}</p>
-
-                        {/* BUTTON */}
-
-                        <button
-                            onClick={() => {
-                            setSelectedAttack({
-                                name: card.name,
-                                damage: player.attack_damage + card.damageBonus,
-                                difficulty: card.difficulty,
-
-                            });
-                            setShowQuiz(true);
-                            }}
-                        >
-                            Attack
-                        </button>
-
                     </div>
+                    <h3>{player.name}</h3>
+
+                    <p className="hp-text">{playerHP} HP</p>
+                </div>
+
+                <div className="vs">VS</div>
+
+                {/* enemy hud */}
+                <div className="hud-enemy">
+            
+                    <div className="hp-bar">
+                        <div
+                            className="hp-fill enemy-hp"
+                            style={{ width: `${enemyHP}%` }}
+                        />
+                    </div>
+                    <h3>{enemy.name}</h3>
+
+                    <p className="hp-text">{enemyHP} HP</p>
+
+                </div>
+
+            </div>
+
+            {/* arena */}
+
+            <div className="arena">
+                <div className="fighter">
+                    <Image
+                        src="/characters/player.png"
+                        alt="Player"
+                        width={300}
+                        height={300}
+                        priority
+                    />
+
+                </div>
+
+                {/* ENEMY */}
+
+                <div className="fighter enemy-fighter">
+
+                    <Image
+                        src="/characters/bill_nye.png"
+                        alt="Enemy"
+                        width={300}
+                        height={300}
+                        priority
+                    />
+
+
+                </div>
+            </div>
+
+            {/* cards */}
+
+            <div className="cards">
+
+                {cards.map((card, index) => (
+
+                <div key={index} className="card outline outline-2 outline-black-400 hover:scale-105 transition">
+
+                    {/* IMAGE */}
+
+                    <Image
+                        src={card.icon}
+                        alt={card.name}
+                        width={100}
+                        height={140}
+
+                    />
+
+                    
+                    <p>{card.name}</p>
+                    <p>{player.attack_damage + card.damageBonus}</p>
+
+                    {/* BUTTON */}
+
+                    <button
+                        onClick={() => {
+                        setSelectedAttack({
+                            name: card.name,
+                            damage: player.attack_damage + card.damageBonus,
+                            difficulty: card.difficulty,
+
+                        });
+                        setShowQuiz(true);
+                        }}
+                    >
+                        Attack
+                    </button>
+
+                </div>
 
                 ))}
                 </div>
