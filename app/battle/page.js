de560ce7b2ff3f080a71340fc8a131569651da5a
Your Name <reannejanelle@outlook.com>
2026-05-09 10:07:25 +1000

    added the battle logic, hp bars, player and enemy, attack cards, and quiz popup

--- app/battle/page.js
+++ app/battle/page.js
@@ -7,7 +7,7 @@
 // quiz popup
 
 "use client";
-import "../../styles/battle.css";
+import "../styles/battle.css";
 
 import { useState } from "react";
 
@@ -19,6 +19,27 @@ export default function BattlePage() {
     const [showQuiz, setShowQuiz] = useState(false);
     const [selectedAttack, setSelectedAttack] = useState(null);
 
+    // answer logic
+    function handleAnswer(correct) {
+        if (correct) {
+            setEnemyHP(enemyHP - selectedAttack.damage);
+        } else {
+            setPlayerHP(playerHP - 15);
+        }
+
+        setShowQuiz(false);
+    }
+
+
+    // add win/lose logic
+    if (enemyHP <= 0) {
+        alert("YOU WIN");
+    }
+
+    if (playerHP <= 0) {
+        alert("YOU LOSE");
+    }
+
     return (
         <div className="battle-page">
             <h1>Slay the Syllabus</h1>
@@ -27,16 +48,26 @@ export default function BattlePage() {
             <div className="arena"></div>
                 <div className="player">
                     {/* add the player image!!!!!! */}
-                    <img src="" alt="player"></img>
-                    <p>Player HP: {playerHP}</p>
+                    {/* <img src="" alt="player"></img> */}
+                    <div className="hp-bar">
+                        <div
+                            className="hp-fill"
+                            style={{ width: `${playerHP}%` }}
+                        ></div>
+                    </div>
                 </div>
 
                 {/* enemy */}
 
                 <div className="enemy">
                     {/* add the player image!!!!!! */}
-                    <img src="" alt="player"></img>
-                    <p>Enemy HP: {enemyHP}</p>
+                    {/* <img src="" alt="player"></img> */}
+                    <div className="hp-bar">
+                        <div
+                            className="hp-fill"
+                            style={{ width: `${playerHP}%` }}
+                        ></div>
+                    </div>
                 </div>
 
                 {/* cards */}
@@ -94,6 +125,33 @@ export default function BattlePage() {
                         Ultimate Attack
                     </button>
                 </div>
+
+                {/* pop up quiz interface */}
+                {showQuiz && (
+                    <div className="quiz-popup">
+                        <h2>Physics Question</h2>
+
+                        <p>What is Newton's Second Law?</p>
+
+                        <div className="answers">
+                        <button onClick={() => handleAnswer(true)}>
+                            F = ma
+                        </button>
+
+                        <button onClick={() => handleAnswer(false)}>
+                            E = mc²
+                        </button>
+
+                        <button onClick={() => handleAnswer(false)}>
+                            V = IR
+                        </button>
+
+                        <button onClick={() => handleAnswer(false)}>
+                            p = mv
+                        </button>
+                        </div>
+                    </div>
+                )}
         </div>
     )
 }
