8f4528b0b344252c3a2482286b2b38acdf2e9105
Barrel-Man <baryalmansoor@gmail.com>
2026-05-09 11:07:52 +1000

    perat of the beging page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,25 @@
-// start page with instructions
 
-import Image from "next/image";
+//export default function Home() {
+ // return (
+    //<main className="flex min-h-screen flex-col items-center justify-between p-24">
+   //   
+    //</main>
+//  );
+//}
+
+"use client";
+import {useRouter} from "next/navigation";
+
+
 
 export default function Home() {
+   const router = useRouter();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-    </main>
+    <div style={{ background: "red", width: "100vw", display: "flex", height: "100vh", flexDirection: "column",
+  alignItems: "center", justifyContent: "center" }}>
+      <button onClick={() => router.push("/battle")}>Play Game</button>
+      <button>Instructions</button>
+    </div>
   );
 }
+
