1e17654426fec726c0606379eacd53c9767378d4
Barrel-Man <baryalmansoor@gmail.com>
2026-05-09 19:33:46 +1000

    thing

---
+++ app/roadmap/route.js
@@ -0,0 +1,12 @@
+"use client";
+
+export default function roadmap() {
+  return (
+    <div style={{ width: "100vw", height: "100vh", background: "black" }}>
+      <img
+        src="/backgrounds/ROADMAP.png"
+        style={{ width: "100%", height: "100%", objectFit: "contain" }}
+      />
+    </div>
+  );
+}
