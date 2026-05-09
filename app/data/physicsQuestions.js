4fcedef6d00529ff84c942cc0bbe100eada10584
Your Name <reannejanelle@outlook.com>
2026-05-10 06:07:57 +1000

    background

---
+++ app/data/physicsQuestions.js
@@ -0,0 +1,8 @@
+// example
+// export const questions = [
+//   {
+//     question: "What is Newton's Second Law?",
+//     answers: ["F=ma", "E=mc²", "V=IR", "p=mv"],
+//     correct: 0,
+//   },
+// ];
