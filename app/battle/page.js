8aa7210a55849319134405fbc6258ab62180a65f
Your Name <reannejanelle@outlook.com>
2026-05-09 10:01:18 +1000

    added the buttons for the cards

--- app/battle/page.js
+++ app/battle/page.js
@@ -16,10 +16,14 @@ export default function BattlePage() {
     const [playerHP, setPlayerHP] = useState(100);
     const [enemyHP, setEnemyHP] = useState(100);
 
+    const [showQuiz, setShowQuiz] = useState(false);
+    const [selectedAttack, setSelectedAttack] = useState(null);
+
     return (
         <div className="battle-page">
             <h1>Slay the Syllabus</h1>
 
+                {/* arena */}
             <div className="arena"></div>
                 <div className="player">
                     {/* add the player image!!!!!! */}
@@ -27,17 +31,68 @@ export default function BattlePage() {
                     <p>Player HP: {playerHP}</p>
                 </div>
 
+                {/* enemy */}
+
                 <div className="enemy">
                     {/* add the player image!!!!!! */}
                     <img src="" alt="player"></img>
                     <p>Enemy HP: {enemyHP}</p>
                 </div>
 
+                {/* cards */}
+
                 <div className="cards">
-                    <button>Light Attack</button>
-                    <button>Medium Attack</button>
-                    <button>Heavy Attack</button>
-                    <button>Ultimate Attack</button>
+                    <button
+                        onClick={() => {
+                            setSelectedAttack({
+                            name: "Light Attack",
+                            damage: 10,
+                            });
+
+                            setShowQuiz(true);
+                        }}
+                        >
+                        Light Attack
+                    </button>
+
+                    <button
+                        onClick={() => {
+                            setSelectedAttack({
+                            name: "Medium Attack",
+                            damage: 10,
+                            });
+
+                            setShowQuiz(true);
+                        }}
+                        >
+                        Medium Attack
+                    </button>
+
+                    <button
+                        onClick={() => {
+                            setSelectedAttack({
+                            name: "Light Attack",
+                            damage: 10,
+                            });
+
+                            setShowQuiz(true);
+                        }}
+                        >
+                        Heavy Attack
+                    </button>
+                    
+                    <button
+                        onClick={() => {
+                            setSelectedAttack({
+                            name: "Ultimate Attack",
+                            damage: 10,
+                            });
+
+                            setShowQuiz(true);
+                        }}
+                        >
+                        Ultimate Attack
+                    </button>
                 </div>
         </div>
     )
