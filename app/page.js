68330c050c095406fcf5ba15e19c144bf6bc567d
Barrel-Man <baryalmansoor@gmail.com>
2026-05-09 11:31:53 +1000

    changes things

--- app/page.js
+++ app/page.js
@@ -1,24 +1,18 @@
-
-//export default function Home() {
- // return (
-    //<main className="flex min-h-screen flex-col items-center justify-between p-24">
-   //   
-    //</main>
-//  );
-//}
-
 "use client";
 import {useRouter} from "next/navigation";
-
+import {useState} from "react";
 
 
 export default function Home() {
    const router = useRouter();
+   const [showInstructions, setShowInstructions] = useState(false);
   return (
     <div style={{ background: "red", width: "100vw", display: "flex", height: "100vh", flexDirection: "column",
   alignItems: "center", justifyContent: "center" }}>
       <button onClick={() => router.push("/battle")}>Play Game</button>
-      <button>Instructions</button>
+      <button onClick={() => setShowInstructions(true)}>Instructions</button>
+      
+      {showInstructions && (<img src="/backgrounds/instrucions.png" onClick={() => setShowInstructions(false)} />)}
     </div>
   );
 }
