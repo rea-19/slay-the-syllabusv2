21fdf5e139a97b6270ffca77ca184fd03d55914e
smano5 <shreyasmanoj.au@gmail.com>
2026-05-10 11:08:55 +1000

    idk bro

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import "./styles/page.css"; 
 
 export default function Home() {
   const router = useRouter();
@@ -12,16 +13,26 @@ export default function Home() {
         src="/backgrounds/another_intro_page.png"
         style={{ width: "100%", height: "100%", objectFit: "contain", pointerEvents: "none" }}
       />
+
+     
+
       <div style={{
         position: "absolute",
         top: "50%",
         left: "50%",
         transform: "translate(-50%, -50%)",
         display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
         gap: "16px",
       }}>
-        <button onClick={() => router.push("/battle")}>Play Game</button>
-        <button onClick={() => setShowInstructions(true)}>Instructions</button>
+
+      <img src="/buttons/play_button.png" onClick={() => router.push("/battle")} className="home-btn" />
+      <img src="/buttons/instructions_button.png" onClick={() => setShowInstructions(true)} className="home-btn" />
+
+        {/* <button onClick={() => router.push("/battle")}>Play Game</button>
+        <button onClick={() => setShowInstructions(true)}>Instructions</button> */}
+      
       </div>
 
       {showInstructions && (
