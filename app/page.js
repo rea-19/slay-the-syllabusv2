70b5343b82ba18a83a889338d796e14a69e7a7dd
Barrel-Man <baryalmansoor@gmail.com>
2026-05-10 10:10:16 +1000

    intro

--- app/page.js
+++ app/page.js
@@ -1,18 +1,36 @@
 "use client";
-import {useRouter} from "next/navigation";
-import {useState} from "react";
-
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Home() {
-   const router = useRouter();
-   const [showInstructions, setShowInstructions] = useState(false);
+  const router = useRouter();
+  const [showInstructions, setShowInstructions] = useState(false);
+
   return (
-    <div style={{ background: "blue", width: "100vw", display: "flex", height: "100vh", flexDirection: "column",
-  alignItems: "center", justifyContent: "center" }}>
-      <button onClick={() => router.push("/battle")}>Play Game</button>
-      <button onClick={() => setShowInstructions(true)}>Instructions</button>
-      
-      {showInstructions && (<img src="/backgrounds/instrucions.png" onClick={() => setShowInstructions(false)} />)}
+    <div style={{ width: "100vw", height: "100vh", background: "black", position: "relative" }}>
+      <img
+        src="/backgrounds/intro_1.png"
+        style={{ width: "100%", height: "100%", objectFit: "contain", pointerEvents: "none" }}
+      />
+      <div style={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        display: "flex",
+        gap: "16px",
+      }}>
+        <button onClick={() => router.push("/battle")}>Play Game</button>
+        <button onClick={() => setShowInstructions(true)}>Instructions</button>
+      </div>
+
+      {showInstructions && (
+        <img
+          src="/backgrounds/instrucions.png"
+          onClick={() => setShowInstructions(false)}
+          style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%" }}
+        />
+      )}
     </div>
   );
 }
